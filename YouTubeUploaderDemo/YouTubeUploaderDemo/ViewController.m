33f5de01dfa8d0fccc2d832a93b13e6f9a67b905
tbago <owner@tbago.com>
2016-04-12 14:41:41 +0800

    complete test code.

--- YouTubeUploaderDemo/YouTubeUploaderDemo/ViewController.m
+++ YouTubeUploaderDemo/YouTubeUploaderDemo/ViewController.m
@@ -31,16 +31,25 @@ static NSString *const kClientSecret    = @"rCcqhEdkZsOkQ4bEEqfM6i47";
 #pragma mark - action
 
 - (IBAction)uploadButtonClick:(UIButton *)sender {
-    NSString *videoFilePath = [[NSBundle mainBundle] pathForResource: @"FCA"
-                                                              ofType: @"mp4"];//Get the Video from Bundle
-    NSURL *videoFileURL = [NSURL fileURLWithPath:videoFilePath];//Convert the NSString To NSURL
+    ///< FixMe(tbago) before use the project
+    ///< drag the video to the project or select video from camera
+    NSString *videoFilePath = [[NSBundle mainBundle] pathForResource:@"FCA"
+                                                              ofType:@"mp4"];   //Get the Video from Bundle
+    NSURL *videoFileURL = [NSURL fileURLWithPath:videoFilePath];    //Convert the NSString To NSURL
     NSData *fileData = [NSData dataWithContentsOfURL:videoFileURL];
     [self.youtubeUploader uploadYoutubeVideo:fileData
                                        title:@"Test video"
                                  description:@"A video for youtube test"
-                                     process:^(float percent) { 
+                                     process:^(float percent) {
+                                         NSLog(@"upload percent:%f", percent);
                                      }
                                     complate:^(BOOL success, NSString *message) {
+                                        if (success) {
+                                            NSLog(@"upload success:%@", message);
+                                        }
+                                        else {
+                                            NSLog(@"%@", message);
+                                        }
                                     }];
 }
 
